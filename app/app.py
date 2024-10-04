f93da6cad5d3e353db28ba700cf7d51ca93894de
pedrostanik <pedro.ostanik@gmail.com>
2024-10-04 09:25:09 -0300

    class SendMessage

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from .send_message import SendMessage
 app = Flask(__name__)
 
 
@@ -14,6 +15,14 @@ def get_webhook():
             return "No JSON payload received", 400  # Tratamento de erro se o JSON estiver vazio
         
         print(f"Payload received in POST: {payload}")
+        if 'text' in payload:
+            message = payload['text']['message']
+            phone = payload['phone']
+            print(f'message: {message}')
+            print(f'phone: {phone}')
+            send_message = SendMessage()
+            answer = f'Mensagem {message} recebida!'
+            send_message.send_message(phone, answer)
         return "POST Message Received", 200  # Sucesso
 
 if __name__ == "__main__":
